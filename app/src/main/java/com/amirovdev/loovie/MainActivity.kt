1c0ca5fbcc31632738fc4ddf4e2da52fe98684aa
Daniyar <dancho.rancho07@gmail.com>
2022-03-30 22:53:31 +0200

    added buttons to the center container, posters to the top bar, text field for posters source, drawable background for the center container, CardView for posters in the top bar, toast-messages when clicking on each button

--- app/src/main/java/com/amirovdev/loovie/MainActivity.kt
+++ app/src/main/java/com/amirovdev/loovie/MainActivity.kt
@@ -2,10 +2,42 @@ package com.amirovdev.loovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initMenuButtons()
+    }
+
+    // show Toast when clicking on the buttons
+    private fun initMenuButtons() {
+        val btnMenu = findViewById<Button>(R.id.btn_menu)
+        btnMenu.setOnClickListener{
+            Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnFavorites = findViewById<Button>(R.id.btn_favorites)
+        btnFavorites.setOnClickListener{
+            Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnWatchLater = findViewById<Button>(R.id.btn_watch_later)
+        btnWatchLater.setOnClickListener{
+            Toast.makeText(this, "Watch later", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnSelections = findViewById<Button>(R.id.btn_selections)
+        btnSelections.setOnClickListener{
+            Toast.makeText(this, "Selections", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+        btnSettings.setOnClickListener{
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+        }
     }
 }
